21fd94623c1002042c8ed5a91a686bdce9261b21
aramis.seitz <aramis.seitz@telekom.de>
2026-04-15 13:15:10 +0000

    paths corrcted

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,12 +1,12 @@
 import express from "express";
-import { getSecureRandomNumber } from "./utils/random";
+import { getSecureRandomNumber } from "./utils/random.js";
 
 const app = express();
 
 const MIN_ROTATION_DEGREE: number = 140;
 const MAX_ROTATION_DEGREE: number = 900;
 
-app.get("/api/random", (req, res) => {
+app.get("/dist/api/random.js", (req, res) => {
     const ranNum = getSecureRandomNumber(MIN_ROTATION_DEGREE, MAX_ROTATION_DEGREE);
     res.json({ ranNum });
 });

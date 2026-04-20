6fcca8d2be3a46ba2f57905c9cec8dda22ab561c
aramis.seitz <aramis.seitz@telekom.de>
2026-04-20 10:26:39 +0000

    sever fix

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,36 +1,15 @@
 import express from "express";
-import path from "path";
-import { getSecureRandomNumber } from "./utils/random";
-
+import { getSecureRandomNumber } from "./utils/random.js";
 
 const app = express();
-const PORT = 3000;
+
 const MIN_ROTATION_DEGREE: number = 140;
 const MAX_ROTATION_DEGREE: number = 900;
 
-
-
-// Pfad zum Frontend
-const frontendPath = path.join(__dirname, "../../public");
-
-// Statische Dateien 
-app.use(express.static(frontendPath));
-
-//  API
-
-app.get("/api/random", (req, res) => {
+app.get("/dist/api/random.js", (req, res) => {
     const ranNum = getSecureRandomNumber(MIN_ROTATION_DEGREE, MAX_ROTATION_DEGREE);
-
     res.json({ ranNum });
 });
- 
- 
-// Fallback: index.html laden
-app.get("*", (req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
-});
 
-app.listen(PORT, () => {
-    console.log(`Server läuft auf http://localhost:${PORT}`);
-});
-
+export default app;
+/*wie kann diese änderung nicht auf dem repo sein hää?*/

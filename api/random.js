78f42f3374723cf5874cd1ba815fb6ade631373f
aramis.seitz <aramis.seitz@telekom.de>
2026-04-23 12:53:23 +0000

    #114 minimale drehung erhöhen

--- api/random.js
+++ api/random.js
@@ -2,7 +2,7 @@ import { getSecureRandomNumber } from "./dist/utils/random.js";
 
 export default function handler(req, res) {
     try {
-        const ranNum = getSecureRandomNumber(140, 900);
+        const ranNum = getSecureRandomNumber(360, 900);
         res.status(200).json({ ranNum });
     } catch (error) {
         res.status(500).json({ error: "Failed to generate number" });

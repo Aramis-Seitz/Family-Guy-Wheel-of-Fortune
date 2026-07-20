d2adf678df75ac3ebeda778aaad2ebc793276376
Ich-de <justusosmers@gmail.com>
2026-07-20 09:34:50 +0200

    fix(ESLint): ignore Mock Files and remove unused variable in file Refs #381

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['**/dist/**', '**/node_modules/**'],
+    ignores: ['**/dist/**', '**/node_modules/**', '**public/script/mock-supabase-client.ts**','**server/src/mock/room/**','**server/src/mock/service.ts**', '**server/src/mock/routes.ts**'],
   },
   {
     files: ['**/*.ts', '**/*.tsx'],

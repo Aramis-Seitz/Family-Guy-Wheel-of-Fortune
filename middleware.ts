03296948cabfadefcea3bda4e563872fb3d99266
lectus76 <winterjarne@gmail.com>
2026-07-29 09:17:14 +0200

    feat(auth): main.html serverseitig vor Auslieferung ohne Login schützen Closes #398

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,18 @@
 import { requireBasicAuthVercel } from './server/src/lib/basic-auth';
+import { requireSessionVercel } from './server/src/lib/session-auth';
 
 export const config = {
     matcher: ['/login.html', '/main.html', '/signup.html'],
 };
 
-export default function middleware(request: Request) {
-    return requireBasicAuthVercel(request);
+export default async function middleware(request: Request): Promise<Response | undefined> {
+    const basicAuthResponse = requireBasicAuthVercel(request);
+    if (basicAuthResponse) return basicAuthResponse;
+
+    const { pathname } = new URL(request.url);
+    if (pathname === '/main.html') {
+        return requireSessionVercel(request, '/login.html');
+    }
+
+    return undefined;
 }
